7af6dd6c5efc64ef3c9ded901565a20fbb6405cf
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-05-18 21:26:30 +0800

    conversation schema

---
+++ src/models/Conversation.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+export const conversationSchema: Schema = new mongoose.Schema({
+  title: String,
+  channelId: String,
+  creatorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+export const Conversation = mongoose.model("Conversation", conversationSchema);
